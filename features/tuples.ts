aaeb83941439d3bdd7742ddae42642e8d36afb31
Paul Calnan <pcalnan71@gmail.com>
2022-09-05 17:19:24 +1000

    typescript tuples

---
+++ features/tuples.ts
@@ -0,0 +1,7 @@
+type Drink = [string, boolean, number];
+
+//const pepsi: [string, boolean, number] = ['brown', true, 40];
+
+const pepsi: Drink = ['brown', true, 40];
+const sprite: Drink = ['clear', true, 40];
+const tea: Drink = ['brown', false, 0];
